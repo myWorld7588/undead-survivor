e054725b15c9fd0c19f7a41ee8b8d03ffe1b2853
myWorld7588 <jin.jakechoi@gmail.com>
2022-12-03 22:45:46 -0500

    New Package Manager
    Input Package Manager
    GetAxis removed
    replaced to OnMove

--- Undead Suvivor/Assets/Undead Survivor/Codes/Player.cs
+++ Undead Suvivor/Assets/Undead Survivor/Codes/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class Player : MonoBehaviour
 {
@@ -13,16 +15,17 @@ public class Player : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.y = Input.GetAxisRaw("Vertical");
-    }
+   
 
     private void FixedUpdate()
     {
 
-        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
     }
 }
